222c174fb4190461612bd4dff037b1b97d8148d9
Marvin Lemos <marvinlemos@gmail.com>
2020-12-10 08:25:27 -0300

    gen cupom

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,7 +1,13 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
 import credentials from '../../credentials.json'
+import moment from 'moment'
 
 const doc = new GoogleSpreadsheet('15lrLBjRMyZ831E_uHaycLSOun3bLIrVjb0GjKS4CW50')
+
+const genCupom = () => {
+    const code = parseInt(moment().format('YYMMDDHHmmssSSS')).toString(16).toUpperCase()
+    return code.substr(0,4) + '-' + code.substr(4,4) + '-' + code.substr(8,4)
+}
 
 export default async(req, res) => {
     try{
@@ -13,17 +19,34 @@ export default async(req, res) => {
 
         const data = JSON.parse(req.body)
 
+        const sheetConfig = doc.sheetsByIndex[2]
+        await sheetConfig.loadCells('A3:B3')
+
+        const showPromotionCell = sheetConfig.getCell(2,0)
+        const messageCell = sheetConfig.getCell(2,1)
+
+        let Cupom = ''
+        let Promo = ''
+
+        if (showPromotionCell.value){
+            //TODO: gerar cupom
+            Cupom = genCupom(),
+            Promo = messageCell.value
+        }
+
         await sheet.addRow({
             Name: data.Name,
             Email: data.Email,
             Whatsapp: '55 86 99999-0001',
-            Cupom: '1111',
-            Promo: 'ABC'
+            Evaluation: 5,
+            'Date': moment().format('DD/MM/YYYY, HH:mm:ss'),
+            Cupom, //Assign the value of the variable with the same name
+            Promo
         })
 
         res.end(req.body)
     }catch(error){
-        console.log('error')
+        console.log('error: ' + error)
         res.end('error')
     }
 }
